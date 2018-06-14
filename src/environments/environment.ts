53219328c1fbe83a94127323b12b519af5224785
phpfzh <phpfzh@phpfzh.com>
2018-06-14 19:43:17 +0800

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  SERVER_URL: `./`,
+  production: false,
+  useHash: false,
 };
 
 /*
@@ -13,3 +15,4 @@ export const environment = {
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
